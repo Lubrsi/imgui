19e82ccd58336b64339f7951d998c7186b446516
Luke <luke.wilde@live.co.uk>
2019-12-11 23:01:54 +0000

    Update premake for linux

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    
+    targetdir ("./bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("./build/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "./imconfig.h",
+        "./imgui.h",
+        "./imgui.cpp",
+        "./imgui_draw.cpp",
+        "./imgui_internal.h",
+        "./imgui_widgets.cpp",
+        "./imstb_rectpack.h",
+        "./imstb_textedit.h",
+        "./imstb_truetype.h",
+        "./imgui_demo.cpp"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+    
+    filter "system:linux"
+        pic "on"
+        systemversion "latest"
+    
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+        optimize "off"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+    filter "configurations:Distribution"
+        runtime "Release"
+        optimize "full"
